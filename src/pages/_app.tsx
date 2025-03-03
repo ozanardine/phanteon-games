1e46e6e276ff5550d5c4b1a6e550767750f06cb0
ozanardine <ozanardine@gmail.com>
2025-03-03 12:29:30 -0300

    Ref 4

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,8 +3,6 @@ import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <AuthProvider>
       <Component {...pageProps} />
-    </AuthProvider>
   );
 }
